53aff1ceca62e0c4e515b6e0442bed27082aefff
Richard Princen <princenrichard@gmail.com>
2017-08-02 15:26:26 +0200

    Changed from Canny to Scharr

--- OpenCVTest/Source.cpp
+++ OpenCVTest/Source.cpp
@@ -13,46 +13,47 @@ using namespace std;
 using namespace cv;
 
 /** Function Headers */
-void detectAndDisplay(Mat frame);
+void detectIris(Mat frame);
 list<int> LBP(Mat iris);
+
 
 /** Global variables */
 String eyes_cascade_name = "haarcascade_eye.xml";
-CascadeClassifier eyes_cascade;
+CascadeClassifier eyes_cascade2;
 string window = "Output";
 RNG rng(12345);
 
-/** @function main */
+
 int main(int argc, const char** argv)
 {
-	Mat frame = imread("face5.jpg");
+	Mat frame = imread("face10.jpg");
 
-	if (!eyes_cascade.load(eyes_cascade_name)) 
+	if (!eyes_cascade2.load(eyes_cascade_name)) 
 	{ 
 		printf("Failed to load cascade\n"); 
 		return -1;
 	};
 
-	detectAndDisplay(frame);
+	detectIris(frame);
 	return 0;
 }
 
-/** @function detectAndDisplay */
-void detectAndDisplay(Mat frame)
+void detectIris(Mat frame)
 {
 	//Eye localization begin
 	std::vector<Rect> faces;
 	Mat frame_gray;
 
 	cvtColor(frame, frame_gray, CV_BGR2GRAY);
-	equalizeHist(frame_gray, frame_gray);
+	//equalizeHist(frame_gray, frame_gray);
 
 	int height = frame.size().height;
 	int minSize = height * 0.15;
 
 	std::vector<Rect> eyes;
-	eyes_cascade.detectMultiScale(frame_gray, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(minSize, minSize));
+	eyes_cascade2.detectMultiScale(frame_gray, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(minSize, minSize));
 	
+	//Rect eyeRegion(eyes[1].x*1.2, eyes[1].y*1.2, eyes[1].width*0.4, eyes[1].height*0.4);
 	Rect eyeRegion(eyes[0].x, eyes[0].y, eyes[0].width, eyes[0].height);
 	frame = frame_gray(eyeRegion);
 
@@ -64,30 +65,56 @@ void detectAndDisplay(Mat frame)
 	//Iris localization begin
 
 	Mat blurredFrame;
-	GaussianBlur(frame, blurredFrame, Size(9, 9), 5, 1);
-	/*imshow(window, blurredFrame);
+	GaussianBlur(frame, blurredFrame, Size(9, 9), 1, 1);
+	imshow(window, blurredFrame);
 	waitKey(0);
-	destroyAllWindows();*/
+	destroyAllWindows();
 
-	/*Mat cannyImage;
-	Mat frame_bw;
-	double highVal = cv::threshold(frame, frame_bw , 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
-	double lowVal = highVal * 0.3;
+	//Mat cannyImage;
+	//Mat frame_bw;
+	//double highVal = threshold(blurredFrame, frame_bw , 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+	//double lowVal = highVal * 0.3;
 
-	cout << "Lower threshold: " << lowVal << endl << "High threshold: " << highVal << endl;
+	//cout << "Lower threshold: " << lowVal << endl << "High threshold: " << highVal << endl;
 
-	Canny(frame, cannyImage, lowVal, highVal, 3, false);
-	imshow("Canny", cannyImage);
+	//Canny(blurredFrame, cannyImage, lowVal, highVal, 3, false);
+	//imshow("Canny", cannyImage);
+	//waitKey(0);
+	//destroyAllWindows();
+
+
+	/// Generate grad_x and grad_y
+	int scale = 1;
+	int delta = 0;
+	int ddepth = CV_16S;
+	Mat processedFrame;
+	Mat grad_x, grad_y;
+	Mat abs_grad_x, abs_grad_y;
+
+	Scharr(blurredFrame, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
+	//Sobel(blurredFrame, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT);
+	convertScaleAbs(grad_x, abs_grad_x);
+
+	Scharr( blurredFrame, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
+	//Sobel(blurredFrame, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT);
+	convertScaleAbs(grad_y, abs_grad_y);
+
+	addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, processedFrame);
+
+	imshow(window, processedFrame);
 	waitKey(0);
-	destroyAllWindows();*/
 
-	int minRadius = blurredFrame.size().height * 0.0725;
-	int maxRadius = blurredFrame.size().height * 0.3;
+	int minRadius = blurredFrame.size().height * 0.15;
+	int maxRadius = blurredFrame.size().height * 0.25;
 	cout << frame.size().height << endl;
 	cout << minRadius << endl;
 
+	Mat frame_bw;
+	double highVal = threshold(processedFrame, frame_bw , 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+	double lowVal = highVal * 0.4;
+
 	vector<Vec3f> circles;
-	HoughCircles(blurredFrame, circles, CV_HOUGH_GRADIENT, 1, frame_gray.rows / 8, 255, 1, minRadius, maxRadius);
+	HoughCircles(processedFrame, circles, CV_HOUGH_GRADIENT, 1, frame_gray.rows / 8, highVal, lowVal, minRadius, maxRadius);
 
 	for (size_t i = 0; i < circles.size(); i++)
 	{
@@ -111,7 +138,7 @@ void detectAndDisplay(Mat frame)
 
 	Rect bbox(circ[0] - circ[2], circ[1] - circ[2], 2 * circ[2], 2 * circ[2]);
 
-	Mat iris(320, 240, CV_8UC3, Scalar(255, 255, 255));
+	Mat iris(200, 200, CV_8UC3, Scalar(255, 255, 255));
 
 	frame.copyTo(iris, mask);
 
@@ -121,16 +148,16 @@ void detectAndDisplay(Mat frame)
 	waitKey(0);
 	//destroyAllWindows();
 
-	list<int> DankMemes = LBP(iris);
+	//list<int> DankMemes = LBP(iris);
 
 	//Pupil location
 	Mat blurredIris;
-	GaussianBlur(iris, blurredIris, Size(9, 9), 3, 1);
+	GaussianBlur(iris, blurredIris, Size(9, 9), 1, 1);
 
 	Mat cannyImage;
-	Mat frame_bw;
-	double highVal = cv::threshold(blurredIris, frame_bw, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
-	double lowVal = highVal * 0.3;
+	frame_bw;
+	highVal = cv::threshold(blurredIris, frame_bw, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+	lowVal = highVal * 0.5;
 
 	cout << "Lower threshold: " << lowVal << endl << "High threshold: " << highVal << endl;
 
@@ -139,7 +166,7 @@ void detectAndDisplay(Mat frame)
 	waitKey(0);
 	//destroyAllWindows();
 
-	int pupilMin = iris.size().height*0.3, pupilMax = iris.size().height*0.5;
+	int pupilMin = iris.size().height*0.05, pupilMax = iris.size().height*0.1;
 
 	HoughCircles(blurredIris, circles, CV_HOUGH_GRADIENT, 1, iris.rows / 8, 255, 1, pupilMin,pupilMax);
 	for (size_t i = 0; i < circles.size(); i++)
